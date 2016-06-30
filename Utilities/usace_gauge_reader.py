8c8767756f6764b11f8d1607ec2a442768828ef7
Jake Ross <jirhiker@gmail.com>
2016-06-30 11:53:30 -0600

    cleaned up gauge reader

--- Utilities/usace_gauge_reader.py
+++ Utilities/usace_gauge_reader.py
@@ -1,10 +1,29 @@
-class readGauges:
+# ===============================================================================
+# Copyright 2016 dgketchum
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+
+from datetime import datetime, timedelta
+import os
+
+
+class USACEGaugeReader:
 
     def __init__(self):
-        None
+        pass
 
     def read_table_rows(self, directory, filename):
-        import os
         fid = open(os.sep.join([directory, filename]))
         print fid
         lines = fid.readlines()
@@ -13,7 +32,6 @@ class readGauges:
         return rows
 
     def fill_hours(self, line):
-        from datetime import datetime, timedelta
         for xx in range(0, 24):
             if line[xx + 3] not in ['m', 'm\n']:
                 day = datetime.strptime(line[2], '%Y%m%d')
@@ -31,23 +49,23 @@ class readGauges:
 
             if filename.endswith('.txt') and filename != 'readme.txt':
 
-                rows = readGauges.read_table_rows(directory, filename)
+                rows = self.read_table_rows(directory, filename)
 
                 for line in rows:
 
                     # '20'  discharge, '76' reservoir inflow
                     if line[0] in ['CLV', 'COY'] and line[1] in ['20', '76']:
-                        q_recs.append(readGauges.fill_hours)
+                        q_recs.append(self.fill_hours(line))
 
                     # '1' is river stage, '6' reservoir stage
                     if line[0] in ['CLV', 'COY'] and line[1] in ['1', '6']:
-                        s_recs.append(readGauges.fill_hours)
+                        s_recs.append(self.fill_hours(line))
 
                     if line[0] == 'COY' and line[1] == '15':  # '15' is res. storage
-                        stor.append(readGauges.fill_hours)
+                        stor.append(self.fill_hours(line))
 
                     if line[0] == 'COY' and line[1] == '23':  # '23' is res. outflow
-                        out_q.append(readGauges.fill_hours)
+                        out_q.append(self.fill_hours(line))
 
         if not stor:
             data = (q_recs, s_recs)
@@ -56,6 +74,7 @@ class readGauges:
             data = (q_recs, s_recs, stor, out_q)
             return data
 
-
     def __str__(self):
         return 'Calculator class'
+
+# ============= EOF =============================================
